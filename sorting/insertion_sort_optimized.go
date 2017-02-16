27a9dd90124e43c355afc9f1b9602ff469a723d2
Huan Jiang <bill.jiangh@gmail.com>
2017-02-16 23:44:52 +0800

    optimized insertion sort

---
+++ sorting/insertion_sort_optimized.go
@@ -0,0 +1,12 @@
+package insertion
+
+func sortOpt(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		unsorted := arr[i]
+		j := i - 1
+		for ; j >= 0 && unsorted < arr[j]; j-- {
+			arr[j+1] = arr[j]
+		}
+		arr[j+1] = unsorted
+	}
+}
---
+++ sorting/insertion_sort_optimized_test.go
@@ -0,0 +1,27 @@
+package insertion
+
+import (
+	"testing"
+
+	"github.com/billjh/algo/sorting/utils"
+)
+
+func TestOptimizedInsertionSort10000(t *testing.T) {
+	arr := utils.GetIntArray(10000)
+	sortOpt(arr)
+	if !utils.IsSorted(arr) {
+		t.Error("array is not sorted:", arr)
+	}
+}
+
+func benchmarkOptimizedInsertionSort(size int, b *testing.B) {
+	arr := utils.GetIntArray(size)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sortOpt(arr)
+	}
+}
+
+func BenchmarkOptimizedInsertionSort100(b *testing.B)   { benchmarkOptimizedInsertionSort(100, b) }
+func BenchmarkOptimizedInsertionSort1000(b *testing.B)  { benchmarkOptimizedInsertionSort(1000, b) }
+func BenchmarkOptimizedInsertionSort10000(b *testing.B) { benchmarkOptimizedInsertionSort(10000, b) }
